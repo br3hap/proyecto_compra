13316feda45a06decd12cc63480ca815b573edc4
Breithner <Breithnerap@gmail.com>
2025-06-29 14:24:12 -0500

    upload change

--- bases/views.py
+++ bases/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin,\
+     PermissionRequiredMixin
+from .models import Idioma, Frase
 
-# Create your views here.
+
+class MixinFormInvalid:
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+        else:
+            return response
+        
+
+class SinPrivilegios(LoginRequiredMixin, PermissionRequiredMixin, MixinFormInvalid):
+    login_url = 'bases:login'
+    raise_exception = False
+    redirect_field_name = 'redirecto_to'
+
+    def handle_no_permission(self):
+        from django.contrib.auth.models import AnonymousUser
+        if not self.request.user == AnonymousUser():
+            self.login_url='bases:sin_provilegios'
+        return HttpResponseRedirect(reverse_lazy(self.login_url))
+    
+
+
+
