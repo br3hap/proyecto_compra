92db5e618ca55c8b9f72c6c1c5163bd4ad055827
Breithner <Breithnerap@gmail.com>
2025-06-29 14:42:40 -0500

    upload change

--- bases/views.py
+++ bases/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse, HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin,\
      PermissionRequiredMixin
+from django.views import generic
 from .models import Idioma, Frase
 
 
@@ -27,5 +28,26 @@ class SinPrivilegios(LoginRequiredMixin, PermissionRequiredMixin, MixinFormInval
         return HttpResponseRedirect(reverse_lazy(self.login_url))
     
 
+class Home(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'bases/home.html'
+    login_url = 'bases:login'
+
+
+class HomeSinPrivilegios(LoginRequiredMixin, generic.TemplateView):
+    login_url = 'bases:login'
+    template_name = 'bases/sin_privilegios.html'
+
+
+class IdiomaList(generic.ListView):
+    template_name = 'bases/idiomas.html'
+    model = Idioma
+    context_object_name = 'obj'
+
+    def get_queryset(self):
+        qs = Frase.objects.all()
+        idioma_id = self.request.GET.get('lang')
+        if idioma_id:
+            qs = qs.filter(idioma__id=idioma_id)
+        return qs
 
 
